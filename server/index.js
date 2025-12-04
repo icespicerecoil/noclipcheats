a6d79ad8a9c77ede7a569dfe87844c9b7e0c68f7
icespicerecoil <138634598+icespicerecoil@users.noreply.github.com>
2025-12-05 02:03:07 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
